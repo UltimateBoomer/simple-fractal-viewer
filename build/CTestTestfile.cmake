7c464ab2c83e7584558bd0529091de7b1a1c94d1
UltimateBoomer <caohc2017@gmail.com>
2021-10-21 16:28:22 -0400

    Delete ignored

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/steven/vscode/gltest
-# Build directory: /home/steven/vscode/gltest/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
